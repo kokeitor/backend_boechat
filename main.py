5f443dc300b0bcc62e899dbedc9cf19ed6858296
jorge <jresino@gmail.com>
2024-09-28 11:48:51 +0200

    e

--- main.py
+++ main.py
@@ -50,31 +50,27 @@ load_dotenv()
 # Set environment variables
 os.environ['LANGCHAIN_TRACING_V2'] = 'true'
 os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
+os.environ['LANGSMITH_API_KEY'] = os.getenv('LANGSMITH')
 os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
 os.environ['PINECONE_API_KEY'] = os.getenv('PINECONE_API_KEY')
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
-os.environ['TAVILY_API_KEY'] = os.getenv('TAVILY_API_KEY')
 os.environ['LLAMA_CLOUD_API_KEY'] = os.getenv('LLAMA_CLOUD_API_KEY')
+os.environ['LLAMA_CLOUD_API_KEY_RAPTOR'] = os.getenv(
+    'LLAMA_CLOUD_API_KEY_RAPTOR')
 os.environ['HF_TOKEN'] = os.getenv('HUG_API_KEY')
 os.environ['PINECONE_INDEX_NAME'] = os.getenv('PINECONE_INDEX_NAME')
-os.environ['CHROMA_COLLECTION_NAME'] = os.getenv('CHROMA_COLLECTION_NAME')
-os.environ['QDRANT_API_KEY'] = os.getenv('QDRANT_API_KEY')
-os.environ['QDRANT_HOST'] = os.getenv('QDRANT_HOST')
-os.environ['QDRANT_COLLECTION_NAME'] = os.getenv('QDRANT_COLLECTION_NAME')
-os.environ['QDRANT_COLLECTIONS'] = os.getenv('QDRANT_COLLECTIONS')
 os.environ['APP_MODE'] = os.getenv('APP_MODE')
-os.environ['NVIDIA_API_KEY'] = os.getenv('NVIDIA_API_KEY')
 os.environ['EMBEDDING_MODEL'] = os.getenv('EMBEDDING_MODEL')
 os.environ['EMBEDDING_MODEL_GPT4'] = os.getenv('EMBEDDING_MODEL_GPT4')
 os.environ['LOCAL_LLM'] = os.getenv('LOCAL_LLM')
-os.environ['LOCAL_LLM'] = os.getenv('GOOGLE_BBDD_FILE_NAME_CREDENTIALS')
-os.environ['GOOGLE_DOCUMENT_NAME'] = os.getenv('GOOGLE_DOCUMENT_NAME')
-os.environ['GOOGLE_SHEET_NAME'] = os.getenv('GOOGLE_SHEET_NAME')
-os.environ['NVIDIA_API_KEY'] = os.getenv('NVIDIA_API_KEY')
+os.environ['HG_REPO_DATASET_ID'] = os.getenv('HG_REPO_DATASET_ID')
+os.environ['HG_REPO_RAGAS_TESTSET_ID'] = os.getenv('HG_REPO_RAGAS_TESTSET_ID')
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
 
 
 def main():
-    subprocess.run(["python", "src/main_etl.py"])
+    print(os.getcwd())
+    subprocess.run(["python", "src/main_etl.py"], check=True)
 
 
 if __name__ == "__main__":
